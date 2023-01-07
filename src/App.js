b8f1ddc6f53097c95b1c96b3f68deae40e0328c2
Koray Guner <okorayguner@gmail.com>
2023-01-07 19:19:36 +0300

    fix: theme state management fix

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import { RouterProvider } from "react-router-dom";
 import Router from "./pages/Router";
-import { useSelector } from 'react-redux'
-import { useEffect } from "react";
 
 function App() {
-
-  const selectedTheme = useSelector(state => state.theme.selectedTheme)
-  
-  useEffect( ()=>{
-    const $html = document.querySelector("html")
-    $html.className = selectedTheme
-  },[selectedTheme])
 
   return (
     <RouterProvider router={Router}/>
--- src/redux/theme/themeSlice.js
+++ src/redux/theme/themeSlice.js
@@ -9,6 +9,8 @@ export const themeSlice = createSlice({
     reducers: {
         changeTheme: (state, action) => {
             state.selectedTheme = action.payload
+            const $html = document.querySelector("html")
+            $html.className = state.selectedTheme
             localStorage.setItem("theme", action.payload)
         }
     }
